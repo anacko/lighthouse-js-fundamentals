77085765485e98ba98278acbe0bd6b799876b611
Ana Carolina K <anacko@gmail.com>
2021-08-24 15:18:38 +0000

    Add function to select good voting stations in Codeville

---
+++ CodevilleElections.js
@@ -0,0 +1,19 @@
+// input
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+// Requirements: Capacity of at least 20 and be a School or Community Centre
+const chooseStations = function (stations) {
+  let goodStations = [];
+  for (let station of stations) {
+    if (station[1] >= 20 && (station[2] == 'school' || station[2] == 'community centre')) {
+      goodStations.push(station[0]);
+    }  
+  }
+  return goodStations;
+}
+
+//console.log(chooseStations(stations));
