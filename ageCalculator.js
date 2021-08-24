21ece769c4f10f738276de9b8f97d641892cf298
Ana Carolina K <anacko@gmail.com>
2021-08-24 18:04:06 +0000

    Add function to calculate the age of people

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+const ageCalculator = function (name, yearOfBirth, currentYear) {
+  return `${name} is ${currentYear - yearOfBirth} years old.`;
+}
+
+//console.log(ageCalculator("Miranda", 1983, 2015)); // Miranda is 32 years old.
+//console.log(ageCalculator("Ferdinand", 1988, 2015)); // Ferdinand is 27 years old.
