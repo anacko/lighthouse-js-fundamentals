f78b875bb44297be23c0e048b1b14d58bfdbcdec
Ana Carolina K <anacko@gmail.com>
2021-08-25 13:30:43 +0000

    Add function to put two arrays together with elements in order

---
+++ merge.js
@@ -0,0 +1,12 @@
+const merge = function (arr1, arr2) {
+  for (let elem of arr2) {
+    arr1.push(elem);
+    arr1.sort();
+  }
+  return arr1;
+};
+/* 
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
+*/
